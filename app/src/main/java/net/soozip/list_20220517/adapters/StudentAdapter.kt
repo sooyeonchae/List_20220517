46d9adae5a52631db314a0e9484bf79a8c2d0fe6
채수연 <sooyeonchae@gmail.com>
2022-05-17 13:14:22 +0900

    getView함수 오버라이딩 - 이론 설명 차후에.

--- app/src/main/java/net/soozip/list_20220517/adapters/StudentAdapter.kt
+++ app/src/main/java/net/soozip/list_20220517/adapters/StudentAdapter.kt
@@ -1,12 +1,29 @@
 package net.soozip.list_20220517.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import net.soozip.list_20220517.R
 import net.soozip.list_20220517.datas.StudentData
 
 class StudentAdapter(
     val mContext: Context,
     val resId: Int,
     val mList: ArrayList<StudentData>) : ArrayAdapter<StudentData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
+        }
+
+        //tempRow는 Null이 들어있을 가능성이 없다.
+        val row = tempRow!! //row가 리스트뷰의 한 줄에 해당하는 그림.(View 타입)
+
+        return row
+    }
 
 }

00c65a031e1c4674ad1b748e7d633ba5ea1e4267
채수연 <sooyeonchae@gmail.com>
2022-05-17 13:48:16 +0900

    실제 데이터 반영

--- app/src/main/java/net/soozip/list_20220517/MainActivity.kt
+++ app/src/main/java/net/soozip/list_20220517/MainActivity.kt
@@ -2,10 +2,37 @@ package net.soozip.list_20220517
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import net.soozip.list_20220517.adapters.StudentAdapter
+import net.soozip.list_20220517.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<StudentData>()
+
+    lateinit var mAdapter : StudentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        mStudentList.add(StudentData("최승철", 1995,"서울시 종로구"))
+        mStudentList.add(StudentData("윤정한", 1995,"서울시 중구"))
+        mStudentList.add(StudentData("홍지수", 1995,"서울시 동대문구"))
+        mStudentList.add(StudentData("문준휘", 1996,"서울시 서대문구"))
+        mStudentList.add(StudentData("권순영", 1996,"서울시 남대문구"))
+        mStudentList.add(StudentData("전원우", 1996,"서울시 강서구"))
+        mStudentList.add(StudentData("이지훈", 1996,"서울시 강남구"))
+        mStudentList.add(StudentData("서명호", 1997,"서울시 강동구"))
+        mStudentList.add(StudentData("김민규", 1997,"서울시 강북구"))
+        mStudentList.add(StudentData("이석민", 1997,"서울시 관악구"))
+        mStudentList.add(StudentData("부승관", 1998,"서울시 은평구"))
+        mStudentList.add(StudentData("최한솔", 1998,"서울시 동작구"))
+        mStudentList.add(StudentData("이찬", 1999,"서울시 마포구"))
+
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+        studentListView.adapter = mAdapter
+
     }
 }
--- app/src/main/java/net/soozip/list_20220517/adapters/StudentAdapter.kt
+++ app/src/main/java/net/soozip/list_20220517/adapters/StudentAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import net.soozip.list_20220517.R
 import net.soozip.list_20220517.datas.StudentData
 
@@ -22,6 +23,26 @@ class StudentAdapter(
 
         //tempRow는 Null이 들어있을 가능성이 없다.
         val row = tempRow!! //row가 리스트뷰의 한 줄에 해당하는 그림.(View 타입)
+
+        //화면에 뿌려지기 전에, row내부를 수정하자.
+        //데이터/반영UI태그 준비.
+        val data = mList[position]
+        val txtName = row.findViewById<TextView>(R.id.txtName)
+        val txtAge = row.findViewById<TextView>(R.id.txtAge)
+        val txtAddress = row.findViewById<TextView>(R.id.txtAddress)
+
+
+        //실제 연결
+        txtName.text = data.name
+        txtAge.text = "${2022-data.birthYear+1}세"
+        txtAddress.text = data.address
+
+
+
+
+
+        //만들어진 row의 그림이 리스트 뷰에 뿌려짐.
+
 
         return row
     }
